507339e1287082d23625053b6bc81b268b8b8b7a
Sumanth H S <sumanthhs48@gmail.com>
2026-03-14 09:18:50 +0530

    initial commit

---
+++ src/test/java/com/example/release_demo/ReleaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.release_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
